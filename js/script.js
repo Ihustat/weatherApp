192f9af5470fe73eb3f230437e1aa3853f15f2c2
Ihustat <ger26dan@gmail.com>
2023-12-06 10:45:22 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,63 +1,84 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const app = document.querySelector('#app'),
+    top = app.querySelector('.top'),
+    extraInfo = app.querySelector('.info'),
+    popup = app.querySelector('.popup'),
+    form = popup.querySelector('.popup__form'),
+    input = popup.querySelector('.popup__input'),
+    errorMessage = popup.querySelector('.error-message'),
+    locatorBtn = document.querySelector('.popup__location-btn'),
+    apiLink =
+      'https://api.weatherapi.com/v1/current.json?key=a01ccd96aca24f04b3b114424231310&q=',
+    weatherParams = {};
 
-    const app = document.querySelector('#app'),
-          top = app.querySelector('.top'),
-          extraInfo = app.querySelector('.info'),
-          popup = app.querySelector('.popup'),
-          form = popup.querySelector('.popup__form'),
-          input = popup.querySelector('.popup__input'),
-          errorMessage = popup.querySelector('.error-message'),
-          locatorBtn = document.querySelector('.popup__location-btn'),
-          apiLink = 'https://api.weatherapi.com/v1/current.json?key=a01ccd96aca24f04b3b114424231310&q=',
-          weatherParams = {};
+  let isFirst = true;
 
-    let isFirst = true;
+  async function getData(city) {
+    const res = await fetch(`${apiLink}${city}&lang=ru`);
 
-    async function getData(city) {
-        const res = await fetch(`${apiLink}${city}&lang=ru`);
+    if (!res.ok) throw new Error();
 
-        if (!res.ok) throw new Error();
+    const result = await res.json();
 
-        const result = await res.json();
+    const {
+      current: {
+        condition,
+        temp_c: temp,
+        feelslike_c: feelsLike,
+        humidity,
+        precip_mm: precip,
+        wind_kph: wind,
+        cloud,
+        last_updated: time,
+        uv,
+        is_day: isDay,
+      },
+      location: { name },
+    } = result;
 
-        const {current:{condition, temp_c: temp, feelslike_c: feelsLike, humidity, precip_mm: precip, wind_kph: wind, cloud, last_updated: time, uv, is_day: isDay}, location:{name}} = result;
-        
-        return {
-            name,
-            condition,
-            temp,
-            time,
-            isDay,
-            info: {feelsLike, humidity, precip, wind, cloud, uv}
-        }
+    return {
+      name,
+      condition,
+      temp,
+      time,
+      isDay,
+      info: { feelsLike, humidity, precip, wind, cloud, uv },
     };
+  }
 
-    function checkParam(param) {
-        switch (param) {
-            case 'feelsLike': return ['Ощущается', '°'];
-            case 'precip': return ['Осадки', 'mm'];
-            case 'humidity': return ['Влажность', '%'];
-            case 'wind': return ['Ветер', 'км/ч'];
-            case 'cloud': return ['Облачность', '%'];
-            case 'uv': return ['УФ индекс', ''];
-            default: return ['Показатель', 'метрика'];
-        };
-    };
+  function checkParam(param) {
+    switch (param) {
+      case 'feelsLike':
+        return ['Ощущается', '°'];
+      case 'precip':
+        return ['Осадки', 'mm'];
+      case 'humidity':
+        return ['Влажность', '%'];
+      case 'wind':
+        return ['Ветер', 'км/ч'];
+      case 'cloud':
+        return ['Облачность', '%'];
+      case 'uv':
+        return ['УФ индекс', ''];
+      default:
+        return ['Показатель', 'метрика'];
+    }
+  }
 
-    function showWeather({
-        name,
-        isDay,
-        condition:{text, icon},
-        temp,
-        info,
-        time}) {
+  function showWeather({
+    name,
+    isDay,
+    condition: { text, icon },
+    temp,
+    info,
+    time,
+  }) {
+    top.innerHTML = '';
+    extraInfo.innerHTML = '';
 
-        top.innerHTML = '';
-        extraInfo.innerHTML = '';
-
-        const elem = `
+    const elem = `
         <div class="city-info">
            Погода в городе
         </div>
@@ -81,11 +102,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     isDay === 0 ? top.classList.add('night') : top.classList.remove('night');
 
-        for (let [key, value] of Object.entries(info)) {
-            
-            const [ruName, metric] = checkParam(key);
-            
-            const e = `
+    for (let [key, value] of Object.entries(info)) {
+      const [ruName, metric] = checkParam(key);
+
+      const e = `
             <div class="info__item">
             <img src="img/${key}.svg" alt="info icon" class="info__item-img">
             <div class="info__item-descr">
@@ -94,81 +114,82 @@ document.addEventListener('DOMContentLoaded', () => {
             </div>
         </div>`;
 
-        extraInfo.insertAdjacentHTML('beforeend', e);
-        };
-    };
+      extraInfo.insertAdjacentHTML('beforeend', e);
+    }
+  }
 
-    function updateWeather(city) {
-        getData(city).then(res => {
-            for (let [key, value] of Object.entries(res)) {
-                weatherParams[key] = value;
-            };
-        })
-        .then(() => {
-            showWeather(weatherParams);
-            errorMessage.style.display = 'none';
-            if (!isFirst) popupClassToggle();
-        })
-        .catch(() => {
-            errorMessage.style.display = 'block';
-            localStorage.setItem('coords', '');
-        })
-        .finally(() => input.value = '');
-    };
-    
-    updateWeather(localStorage.getItem('coords') || 'Москва');
+  function updateWeather(city) {
+    getData(city)
+      .then((res) => {
+        for (let [key, value] of Object.entries(res)) {
+          weatherParams[key] = value;
+        }
+      })
+      .then(() => {
+        showWeather(weatherParams);
+        errorMessage.style.display = 'none';
+        if (!isFirst) popupClassToggle();
+      })
+      .catch(() => {
+        errorMessage.style.display = 'block';
+        localStorage.setItem('coords', '');
+      })
+      .finally(() => (input.value = ''));
+  }
 
+  updateWeather(localStorage.getItem('coords') || 'Москва');
 
-    function popupClassToggle() {
-        popup.classList.toggle('active');
-        isFirst = false;
+  function popupClassToggle() {
+    popup.classList.toggle('active');
+    isFirst = false;
 
-        document.documentElement.style.overflow ? document.documentElement.style.overflow = '' : document.documentElement.style.overflow = 'hidden';
+    document.documentElement.style.overflow
+      ? (document.documentElement.style.overflow = '')
+      : (document.documentElement.style.overflow = 'hidden');
+  }
 
-    };
+  function popupClassToggleHandler(e) {
+    const target = e.target;
 
-    function popupClassToggleHandler(e) {
-        const target = e.target;
+    if (
+      target &&
+      (target.classList.contains('city-name') ||
+        target.classList.contains('popup__close'))
+    ) {
+      popupClassToggle();
+    }
+  }
 
-        if (target && (target.classList.contains('city-name') || target.classList.contains('popup__close'))) {
-            popupClassToggle();
-        };
-    };
+  function formSubmitHandler(e) {
+    e.preventDefault();
 
-    function formSubmitHandler(e) {
-        e.preventDefault();
+    const newCity = input.value;
 
-        const newCity = input.value;
+    localStorage.setItem('coords', input.value);
 
-        localStorage.setItem('coords', input.value);
+    updateWeather(newCity);
+  }
 
-        updateWeather(newCity);
-    };
+  function locatorHandler() {
+    function succes(position) {
+      const long = position.coords.longitude;
+      const lat = position.coords.latitude;
 
-    function locatorHandler() {
+      localStorage.setItem('coords', `${lat},${long}`);
 
-        function succes(position) {
-            const long = position.coords.longitude;
-            const lat = position.coords.latitude;
+      updateWeather(`${lat},${long}`);
+    }
 
+    function failure() {
+      alert('Вы запретили испоьзовать данные местоположения');
+    }
 
-            localStorage.setItem('coords', `${lat},${long}`);
+    navigator.geolocation.getCurrentPosition(succes, failure);
+  }
 
-            updateWeather(`${lat},${long}`);
-        };
+  app.addEventListener('click', (e) => popupClassToggleHandler(e));
 
-        function failure() {
-            alert('Вы запретили испоьзовать данные местоположения');
-        };
-        
-        navigator.geolocation.getCurrentPosition(succes, failure)
-    };
+  form.addEventListener('submit', (e) => formSubmitHandler(e));
 
-
-    app.addEventListener('click', (e) => popupClassToggleHandler(e));
-
-    form.addEventListener('submit', (e) => formSubmitHandler(e));
-
-    locatorBtn.addEventListener('click', locatorHandler);
-
+  locatorBtn.addEventListener('click', locatorHandler);
 });
